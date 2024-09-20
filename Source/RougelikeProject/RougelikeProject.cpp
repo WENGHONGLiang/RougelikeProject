b2879c9e19689ad2acf7a30acc81d1e227efbe21
WENGHONGLiang <794455789@qq.com>
2024-09-20 19:31:09 +0800

    init

---
+++ Source/RougelikeProject/RougelikeProject.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RougelikeProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RougelikeProject, "RougelikeProject" );
+
+DEFINE_LOG_CATEGORY(LogRougelikeProject)
+ 
---
+++ Source/RougelikeProject/RougelikeProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RougelikeProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARougelikeProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARougelikeProjectGameMode();
+};
+
+
+
