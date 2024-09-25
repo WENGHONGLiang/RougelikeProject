3c3e9219809258b5c362b26256045ad71ded25b0
GotMebbudysheee <1127644819@qq.com>
2024-09-25 18:00:14 +0800

    9.25 安装C++插件：Message

---
+++ Source/RougelikeProject/BPFL/BPFL_Math.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BPFL_Math.h"
+
+void UBPFL_Math::IntPlusEquals(int32& Int, int32 Add, int32& IntOut) {
+	Int += Add;
+	IntOut = Int;
+}
+
+void UBPFL_Math::IntMinusEquals(int32& Int, int32 Sub, int32& IntOut) {
+	Int -= Sub;
+	IntOut = Int;
+}
+
+void UBPFL_Math::IntMultiplyEquals(int32& Int, int32 Mul, int32& IntOut) {
+	Int *= Mul;
+	IntOut = Int;
+}
+
+void UBPFL_Math::IntDivideEquals(int32& Int, int32 Div, int32& IntOut) {
+	Int /= Div;
+	IntOut = Int;
+}
+
+void UBPFL_Math::FloatPlusEquals(float& Float, float Add, float& FloatOut) {
+	Float += Add;
+	FloatOut = Float;
+}
+
+void UBPFL_Math::FloatMinusEquals(float& Float, float Sub, float& FloatOut) {
+	Float -= Sub;
+	FloatOut = Float;
+}
+
+void UBPFL_Math::FloatMultiplyEquals(float& Float, float Mul, float& FloatOut) {
+	Float *= Mul;
+	FloatOut = Float;
+}
+
+void UBPFL_Math::FloatDivideEquals(float& Float, float Div, float& FloatOut) {
+	Float /= Div;
+	FloatOut = Float;
+}
+
+float UBPFL_Math::CalculateRadius(const float SemiMajorAxis, const float SemiMinorAxis, const float AngleInRadians) {
+	{
+		const float CosAngle = FMath::Cos(AngleInRadians); // 计算角度的余弦值
+		const float SinAngle = FMath::Sin(AngleInRadians); // 计算角度的正弦值
+
+		const float Numerator = SemiMajorAxis * SemiMinorAxis; // 计算公式中的分子
+		const float Denominator = FMath::Sqrt(
+			FMath::Square(SemiMinorAxis * CosAngle) + FMath::Square(SemiMajorAxis * SinAngle)); // 计算公式中的分母
+		const float Radius = Numerator / Denominator; // 计算半径
+
+		return Radius;
+	}
+}
---
+++ Source/RougelikeProject/BPFL/BPFL_Math.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BPFL_Math.generated.h"
+
+/**
+ * 数学库
+ */
+UCLASS(Blueprintable)
+class UBPFL_Math : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+
+public:
+	/** int += */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "+=", Keywords = "increment integer"),
+		Category = "BPFL|Math|Integer")
+	static void IntPlusEquals(UPARAM(ref) int32& Int, int32 Add, int32& IntOut);
+	/** int -= */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "-=", Keywords = "decrement integer"),
+		Category = "BPFL|Math|Integer")
+	static void IntMinusEquals(UPARAM(ref) int32& Int, int32 Sub, int32& IntOut);
+	/** int *= */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "*=", Keywords = "decrement integer"),
+		Category = "BPFL|Math|Integer")
+	static void IntMultiplyEquals(UPARAM(ref) int32& Int, int32 Mul, int32& IntOut);
+	/** int /= */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "/=", Keywords = "decrement integer"),
+		Category = "BPFL|Math|Integer")
+	static void IntDivideEquals(UPARAM(ref) int32& Int, int32 Div, int32& IntOut);
+	/** float += */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "+=", Keywords = "increment float"),
+		Category = "BPFL|Math|Float")
+	static void FloatPlusEquals(UPARAM(ref) float& Float, float Add, float& FloatOut);
+	/** float -= */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "-=", Keywords = "decrement float"),
+		Category = "BPFL|Math|Float")
+	static void FloatMinusEquals(UPARAM(ref) float& Float, float Sub, float& FloatOut);
+	/** float *= */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "*=", Keywords = "decrement float"),
+		Category = "BPFL|Math|Float")
+	static void FloatMultiplyEquals(UPARAM(ref) float& Float, float Mul, float& FloatOut);
+	/** float /= */
+	UFUNCTION(BlueprintCallable, meta = (CompactNodeTitle = "/=", Keywords = "decrement float"),
+		Category = "BPFL|Math|Float")
+	static void FloatDivideEquals(UPARAM(ref) float& Float, float Div, float& FloatOut);
+	/**
+	 * 计算椭圆在某个角度上的点到中心的距离（半径
+	 * @param SemiMajorAxis 长轴
+	 * @param SemiMinorAxis 短轴
+	 * @param AngleInRadians 角度
+	 * @return 距离（半径
+	 */
+	static float CalculateRadius(const float SemiMajorAxis, const float SemiMinorAxis, const float AngleInRadians);
+};
